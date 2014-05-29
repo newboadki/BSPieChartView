e35d8ebe3e5604cb21d93a68e3e6bd1ff61e8d78
Borja Arias Drake <borja.arias@gmail.com>
2014-05-29 22:03:06 +0100

    Initial Commit

---
+++ BSPieChartView/BSPieChartView.m
@@ -0,0 +1,13 @@
+//
+//  BSPieChartView.m
+//  BSPieChartView
+//
+//  Created by Borja Arias Drake on 29/05/2014.
+//  Copyright (c) 2014 Borja Arias Drake. All rights reserved.
+//
+
+#import "BSPieChartView.h"
+
+@implementation BSPieChartView
+
+@end
---
+++ BSPieChartViewTests/BSPieChartViewTests.m
@@ -0,0 +1,34 @@
+//
+//  BSPieChartViewTests.m
+//  BSPieChartViewTests
+//
+//  Created by Borja Arias Drake on 29/05/2014.
+//  Copyright (c) 2014 Borja Arias Drake. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface BSPieChartViewTests : XCTestCase
+
+@end
+
+@implementation BSPieChartViewTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
